277c61a9743836567c7086c5798a2ddee7efa103
Siho <siho8808@gmail.com>
2023-06-15 15:41:42 +0900

    password 가림

--- routes/post.js
+++ routes/post.js
@@ -5,7 +5,19 @@ const Post = require('../schemas/post.js');
 // 게시글 전체 조회 API
 router.get('/posts', async (req, res) => {
   // const cursor = await Post.find();
-  const posts = await Post.find().sort({ syncTime: -1 });
+  const posts = await Post.find(
+    {},
+    {
+      _id: true,
+      postId: true,
+      title: true,
+      author: true,
+      content: true,
+      syncTime: true,
+    }
+  ).sort({ syncTime: -1 });
+  console.log(posts);
+  // .select('postId title author content syncTime');
   // find(query, projection)는 인자가 없으면 모든 도큐먼트를 조회, 리턴값은 cursor 객체.
   // query는 조회 기준, projection은 조회한 도큐먼트를 표시할 필드.
   // const posts = cursor.sort({ syncTime: -1 });
@@ -17,8 +29,22 @@ router.get('/posts', async (req, res) => {
 router.get('/posts/:postId', async (req, res) => {
   const { postId } = req.params;
   // console.log(postId);
-  const post = await Post.findOne({ postId });
-  //find()는 배열 형태, findOne() 은 객체 형태
+  const post = await Post.findOne(
+    { postId },
+    {
+      _id: true,
+      postId: true,
+      title: true,
+      author: true,
+      content: true,
+      syncTime: true,
+    }
+  );
+  // console.log(post);
+
+  // const test = await Post.find({ postId });
+  // console.log(test);
+  //find()는 배열안에 객체형태, findOne() 은 객체 형태
   if (!post) {
     return res.status(404).json({ message: '게시글을 찾을 수 없습니다.' });
   }
